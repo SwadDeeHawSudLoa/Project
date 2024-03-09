f9057558c2c1107ac187e4542a50791ad62fab14
SwadDeeHawSudLoa <127951003+SwadDeeHawSudLoa@users.noreply.github.com>
2024-03-10 01:40:09 +0700

    ubgrade login UI

--- mycleaningproject/app/login/page.tsx
+++ mycleaningproject/app/login/page.tsx
@@ -6,38 +6,45 @@ export default function Home() {
   return (
     <>
       <NavBar />
-      <div className='bg-slate-300'>
-      <div className="flex justify-center items-center h-screen w-auto ">
-      <div className="rounded-lg shadow-xl bg-red-200  sm:w-1/10 md:w-5/12 w-1/2 p-4">
-        <div className="flex flex-col items-center">
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-700 mb-2">Username</label>
-            <input
-              className="w-full bg-white-400 shadow p-2 rounded-md"
-              type="text"
-              placeholder="Username"
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-medium text-gray-700 mb-2">Password</label>
-            <input
-              className="w-full bg-white-400 shadow p-2 rounded-md"
-              type="text"
-              placeholder="Password"
-            />
-          </div>
-          <div >
-            <button className='hover:font-bold  hover:bg-orange-600 sm:flex sm:items-center sm:dark:bg-yellow-500 sm:rounded-lg max-w-screen-xl flex items-center justify-between mx-auto p-2 md:dark:bg-yellow-500 mt-5 rounded-lg shadow-xl dark:bg-yellow-500 w-full '>Login</button>
-            
+      <section className="bg-gray-50 dark:bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+            <img className="w-8 h-8 mr-2" src="https://s3.getstickerpack.com/storage/uploads/sticker-pack/keqing/sticker_7.png?4996771f86fd38e818153e52edcc458f"  alt="logo" />
+            TU seek and find
+          </a>
+          <div className="shadow-2xl w-full bg-orange-300 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Sign in to your account
+              </h1>
+              <form className="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                  <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                </div>
+                <div>
+                  <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                  <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                </div>
+                <div className="flex items-center justify-between">
+                  <div className="flex items-start">
+                    <div className="flex items-center h-5">
+                      <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required />
+                    </div>
+                    <div className="ml-3 text-sm">
+                      <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                    </div>
+                  </div>
+                  <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                </div>
+                <button type="submit" className="w-full text-white bg-red-400 hover:bg-violet-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-    </div>
-      
-  
-
+      </section>
     </>
   );
 }
+
 
